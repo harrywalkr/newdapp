f0aa2fa7eef71533023b47a2c00f49e0bc9d04d4
ahmad baghereslami <ahmad.b1995@gmail.com>
2024-04-01 21:15:39 +0330

    initial commit + header

--- http/token.http.ts
+++ http/token.http.ts
@@ -1,9 +1,9 @@
 import { req, res } from "@/types/http.type";
-import { Spotlight } from "@/types/features/spotlight.type";
+import { SpotlightSearchType } from "@/types/features/spotlight.type";
 import { Token } from "@/types/token.type";
 import axiosInstance from "./axios.config";
 
-export const spotlightSearch = (config: req): res<Spotlight> => {
+export const spotlightSearch = (config: req): res<SpotlightSearchType> => {
   return axiosInstance.get(`${process.env.NEXT_PUBLIC_BASE_URL_ONE}/search`, {
     params: config.params,
   });
