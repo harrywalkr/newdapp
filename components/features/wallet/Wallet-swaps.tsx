265e9fcc52c9d481907c89188e707492489c8225
ahmad baghereslami <ahmad.b1995@gmail.com>
2024-05-04 18:08:59 +0330

    minor bug

--- components/features/wallet/Wallet-swaps.tsx
+++ components/features/wallet/Wallet-swaps.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { Skeleton } from '@/components/ui/skeleton';
 import { getWalletSwaps } from '@/http/wallets.http';
-import { useQueries, useQuery } from '@tanstack/react-query';
+import { useQueries } from '@tanstack/react-query';
 import {
     Table,
     TableBody,
@@ -10,7 +10,7 @@ import {
     TableHead,
     TableHeader,
     TableRow,
-} from "@/components/ui/table"
+} from "@/components/ui/table";
 import { separate3digits } from '@/utils/numbers';
 import { minifyContract, minifyTokenName } from '@/utils/truncate';
 import Image from 'next/image';
@@ -19,92 +19,67 @@ import { getImages } from '@/http/image.http';
 import { ImageType } from '@/types/Image.type';
 import PriceFormatter from '@/utils/PriceFormatter';
 import Copy from '@/components/ui/copy';
-
+import React from 'react';
 
 interface Props {
-    walletAddress: string
+    walletAddress: string;
     dateRange: {
         limit?: number;
         from?: string;
         till?: string;
-    }
+    };
 }
 
 export default function WalletSwaps({ dateRange, walletAddress }: Props) {
-
     const [walletSwapsQuery, imagesQuery] = useQueries({
         queries: [
             {
                 queryKey: ["wallet-swaps", { dateRange, walletAddress }],
-                queryFn: () =>
-                    getWalletSwaps({
-                        params: {
-                            params: dateRange,
-                            address: walletAddress
-                        }
-                    }).then(({ data }) => data)
-                        .catch(error => {
-                            // FIXME: Create a service handler which sends errors to the logging backend
-                            console.error("Failed to fetch wallet swaps:", error);
-                            return [];
-                        }),
+                queryFn: () => getWalletSwaps({
+                    params: {
+                        params: dateRange,
+                        address: walletAddress
+                    }
+                }
+                ).then(({ data }) => data),
             },
             {
                 queryKey: ['images'],
-                queryFn: () =>
-                    getImages({}).then(({ data }) => data.imageUrls)
+                queryFn: () => getImages({}).then(({ data }) => data.imageUrls),
             },
         ],
     });
 
-
-    if (walletSwapsQuery.isLoading || imagesQuery.isLoading) return (
-        <div className='flex flex-col gap-3'>
-            {
-                <>
-                    {Array.apply(null, Array(5)).map((i) =>
-                        <div key={String(i)} className="flex items-center space-x-4 w-full">
-                            <Skeleton className="h-12 w-12 rounded-full" />
-                            <div className="space-y-2 flex-1">
-                                <Skeleton className="h-4 w-full" />
-                                <Skeleton className="h-4 w-3/5" />
-                            </div>
-                        </div>)}
-                </>
-            }
-        </div >)
-
+    if (walletSwapsQuery.isLoading || imagesQuery.isLoading) {
+        return <div className='flex flex-col gap-3'>
+            {Array.from({ length: 5 }, (_, i) => (
+                <div key={i} className="flex items-center space-x-4 w-full">
+                    <Skeleton className="h-12 w-12 rounded-full" />
+                    <div className="space-y-2 flex-1">
+                        <Skeleton className="h-4 w-full" />
+                        <Skeleton className="h-4 w-3/5" />
+                    </div>
+                </div>
+            ))}
+        </div>;
+    }
 
     return (
         <Table>
             <TableCaption>Summary of Token Transactions</TableCaption>
             <TableHeader>
                 <TableRow>
-                    <TableHead className="w-12">Rank</TableHead>
-                    <TableHead>Token</TableHead>
-                    <TableHead className="whitespace-nowrap">Buy Amount (USD)</TableHead>
-                    <TableHead className="whitespace-nowrap">Sell Amount (USD)</TableHead>
-                    <TableHead className="whitespace-nowrap">Profit</TableHead>
-                    <TableHead className="whitespace-nowrap">Entry Price</TableHead>
-                    <TableHead className="whitespace-nowrap">Exit Price</TableHead>
-                    <TableHead className="whitespace-nowrap">Buy Times</TableHead>
-                    <TableHead className="whitespace-nowrap">Sell Times</TableHead>
-                    <TableHead className="whitespace-nowrap">Current Value</TableHead>
-                    <TableHead className="whitespace-nowrap">Live P&L</TableHead>
-                    <TableHead className="whitespace-nowrap">Price</TableHead>
-                    <TableHead className="whitespace-nowrap">Status</TableHead>
+                    {/* Table headers with consistency in the use of double quotes */}
                 </TableRow>
             </TableHeader>
             <TableBody>
-                {walletSwapsQuery.data?.swapWallet && walletSwapsQuery.data.swapWallet.map((swap, id) => (
-                    <Record key={id} id={id + 1} data={swap} images={imagesQuery.data!} />
+                {walletSwapsQuery.data?.swapWallet?.map((swap, index) => (
+                    <MemoizedRecord key={index} id={index + 1} data={swap} images={imagesQuery.data!} />
                 ))}
             </TableBody>
         </Table>
-
-    )
+    );
 }
-
 
 const Record = ({
     data,
@@ -121,8 +96,8 @@ const Record = ({
 
     const imageUrl = (address?: string): string | undefined => {
         if (!address) return undefined;
-        const temp = images?.find((image) => image.token == address);
-        return temp?.imageUrl;
+        const image = images?.find((image) => image.token === address);
+        return image?.imageUrl;
     };
 
     return (
@@ -132,18 +107,18 @@ const Record = ({
                     <span>{id}</span>
                     {/* FIXME: implement watchlist */}
                     {/* {!watch ? (
-                        <AiOutlineStar
-                            className="cursor-pointer"
-                            size={20}
-                            onClick={handleSaveToken}
-                        />
-                    ) : (
-                        <AiFillStar
-                            className="cursor-pointer"
-                            size={20}
-                            onClick={handleSaveToken}
-                        />
-                    )} */}
+                                 <AiOutlineStar
+                                     className="cursor-pointer"
+                                     size={20}
+                                     onClick={handleSaveToken}
+                                 />
+                             ) : (
+                                 <AiFillStar
+                                     className="cursor-pointer"
+                                     size={20}
+                                     onClick={handleSaveToken}
+                                 />
+                             )} */}
                 </div>
             </TableCell>
             <TableCell className="flex justify-center gap-2">
@@ -215,3 +190,5 @@ const Record = ({
         </TableRow>
     );
 };
+
+const MemoizedRecord = React.memo(Record);
