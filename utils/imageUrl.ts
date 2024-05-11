75ee10aae386224b2d4c6d52943b48331b709091
ahmad baghereslami <ahmad.b1995@gmail.com>
2024-05-12 02:20:28 +0330

    vercel build

--- utils/imageUrl.ts
+++ utils/imageUrl.ts
@@ -1,10 +1,10 @@
 import { ImageType } from "@/types/Image.type";
 
 export const imageUrl = (
-  address?: string,
+  address: string,
   images: ImageType[]
 ): string | undefined => {
   if (!address) return undefined;
-  const image = images?.find((image) => image.token === address);
+  const image = images.find((image) => image.token === address);
   return image?.imageUrl;
 };
