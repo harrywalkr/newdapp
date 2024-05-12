7ba32846ad2b67abe639d0e77c1a08f3e3139334
ahmad baghereslami <ahmad.b1995@gmail.com>
2024-05-12 21:09:05 +0330

    minor

--- components/features/landing-page/Hero.tsx
+++ components/features/landing-page/Hero.tsx
@@ -4,11 +4,11 @@ import { GitHubLogoIcon } from "@radix-ui/react-icons";
 
 export const Hero = () => {
   return (
-    <section className="container grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10">
+    <section className="container grid lg:grid-cols-2 place-items-center pt-20 pb-10 md:pb-20 gap-10">
       <div className="text-center lg:text-start space-y-6">
         <main className="text-5xl md:text-6xl font-bold">
           <h2 className="inline">
-            <span className="inline bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] text-transparent bg-clip-text">
+            <span className="inline bg-gradient-to-r text-brand bg-clip-text">
             DexTrading landing page for DEX
             traders
             </span>
