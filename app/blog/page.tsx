5e34649e4f7fd935d32398deaa0be51004fcbd1c
ahmad baghereslami <ahmad.b1995@gmail.com>
2024-05-17 21:13:45 +0330

    test

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -6,12 +6,12 @@ import { getAllCategories } from '@/services/http/blog/category.http';
 export default async function Blog() {
     // FIXME: implement seo meta data to all pages
 
-    const posts = await getAllPosts()
-    const categories = await getAllCategories()
+    // const posts = await getAllPosts()
+    // const categories = await getAllCategories()
 
     return (
         <div>
-            <BlogHomepage posts={posts} categories={categories} />
+            {/* <BlogHomepage posts={posts} categories={categories} /> */}
         </div>
     )
 }
