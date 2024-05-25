4b872378e716d8b93cbaf89feb13f211eeff7f7e
ahmad baghereslami <ahmad.b1995@gmail.com>
2024-05-25 14:31:22 +0330

    minor

--- components/features/dashboard/dashboard/components/overview.tsx
+++ components/features/dashboard/dashboard/components/overview.tsx
@@ -1,61 +1,61 @@
-'use client';
+"use client";
+import {
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LineElement,
+  LinearScale,
+  PointElement,
+  Title,
+  Tooltip,
+} from "chart.js";
 import { useEffect, useState } from "react";
 import { Line } from "react-chartjs-2";
 import { chartHeight, chartOptions } from "@/utils/animation";
 
-let ChartJS;
-if (typeof window !== "undefined") {
-  import("chart.js").then((module) => {
-    ChartJS = module.Chart;
-    ChartJS.register(
-      module.CategoryScale,
-      module.LinearScale,
-      module.PointElement,
-      module.LineElement,
-      module.Title,
-      module.Tooltip,
-      module.Legend
-    );
-  });
-}
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 export function Overview({ walletInfo }: any) {
   const [history, setHistory] = useState<any>({});
 
   useEffect(() => {
-    if (typeof window !== "undefined" && walletInfo && walletInfo.totalProfits && walletInfo.totalProfits.month) {
-      const labels = Object.keys(walletInfo.totalProfits.month).reverse();
-      const data = Object.values(walletInfo.totalProfits.month).reverse();
-
-      setHistory({
-        labels,
-        datasets: [
-          {
-            data,
-            borderColor: walletInfo.netProfit > 0 ? 'rgb(54, 211, 153)' : 'rgb(255, 99, 132)',
-            backgroundColor: walletInfo.netProfit > 0 ? 'rgba(54, 211, 153, 0.5)' : 'rgba(255, 99, 132, 0.5)',
-            fill: true,
-            tension: 0.4,
-            cubicInterpolationMode: 'monotone',
-            pointRadius: 3,
-            pointHoverRadius: 10,
-          },
-        ],
-      });
-    }
+    const labels = Object.keys(walletInfo.totalProfits.month).reverse();
+    setHistory({
+      labels,
+      datasets: [
+        {
+          data: Object.values(walletInfo.totalProfits.month).reverse(),
+          borderColor: walletInfo.netProfit > 0 ? "rgb(54, 211, 153)" : "rgb(255, 99, 132)",
+          backgroundColor: walletInfo.netProfit > 0 ? "rgb(54, 211, 153, 0.5)" : "rgb(255, 99, 132, 0.5)",
+          fill: true,
+          tension: 0.4,
+          cubicInterpolationMode: "monotone",
+          pointRadius: 3,
+          pointHoverRadius: 10,
+        },
+      ],
+    });
   }, [walletInfo]);
 
-  if (!history.datasets) return null; // Handle the case where history is not set
+  if (!history.datasets) return;
 
   return (
     <div className="mt-10 w-full min-h-[300px] overflow-hidden flex flex-col justify-center items-center">
-      {/* <Line
+      <Line
         data={history}
         className="w-full"
         width={1248}
         height={chartHeight}
         options={chartOptions()}
-      /> */}
+      />
     </div>
   );
 }
