794b2ec53d046854061a7493d587bca789d40f91
ahmad baghereslami <ahmad.b1995@gmail.com>
2024-05-25 14:30:21 +0330

    minor

--- components/features/dashboard/dashboard/components/overview.tsx
+++ components/features/dashboard/dashboard/components/overview.tsx
@@ -20,11 +20,42 @@ if (typeof window !== "undefined") {
 }
 
 export function Overview({ walletInfo }: any) {
+  const [history, setHistory] = useState<any>({});
 
+  useEffect(() => {
+    if (typeof window !== "undefined" && walletInfo && walletInfo.totalProfits && walletInfo.totalProfits.month) {
+      const labels = Object.keys(walletInfo.totalProfits.month).reverse();
+      const data = Object.values(walletInfo.totalProfits.month).reverse();
+
+      setHistory({
+        labels,
+        datasets: [
+          {
+            data,
+            borderColor: walletInfo.netProfit > 0 ? 'rgb(54, 211, 153)' : 'rgb(255, 99, 132)',
+            backgroundColor: walletInfo.netProfit > 0 ? 'rgba(54, 211, 153, 0.5)' : 'rgba(255, 99, 132, 0.5)',
+            fill: true,
+            tension: 0.4,
+            cubicInterpolationMode: 'monotone',
+            pointRadius: 3,
+            pointHoverRadius: 10,
+          },
+        ],
+      });
+    }
+  }, [walletInfo]);
+
+  if (!history.datasets) return null; // Handle the case where history is not set
 
   return (
     <div className="mt-10 w-full min-h-[300px] overflow-hidden flex flex-col justify-center items-center">
-   
+      {/* <Line
+        data={history}
+        className="w-full"
+        width={1248}
+        height={chartHeight}
+        options={chartOptions()}
+      /> */}
     </div>
   );
 }
