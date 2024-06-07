dd5156cd2f438f04fb44d5490239c41aadbf75b4
ahmad baghereslami <ahmad.b1995@gmail.com>
2024-06-07 11:31:01 +0330

    minor

--- components/features/homepage/Insight.tsx
+++ components/features/homepage/Insight.tsx
@@ -1,15 +1,15 @@
 'use client'
 import { Section, SectionContent, SectionDescription, SectionHeader, SectionTitle } from '@/components/layout/Section';
 import { Avatar, AvatarFallback } from '@/components/ui/avatar';
-import { Card, CardContent, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import Copy from '@/components/ui/copy';
+import { Separator } from '@/components/ui/separator';
 import { Skeleton } from '@/components/ui/skeleton';
 import { getAi } from '@/services/http/token.http';
 import { IWallet } from '@/types/Wallet.type'
 import { IAI } from '@/types/token.type';
 import { separate3digits } from '@/utils/numbers';
 import { minifyContract } from '@/utils/truncate';
-import { CardHeader } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
 import clsx from 'clsx';
 import React from 'react'
@@ -32,14 +32,14 @@ export default function Insight({ wallets }: Props) {
     )
 
     if (error) return <div>Failed to load ai trends, please try again.</div>;
-    if (isLoading) return <Section>
+    if (isLoading) return <Section variant='vertical'>
         <SectionHeader variant='vertical'>
             <SectionTitle>Insights & Analytics</SectionTitle>
             <SectionDescription>
                 Transform data into actionable intelligence for smarter decisions and strategic growth.
             </SectionDescription>
         </SectionHeader>
-        <SectionContent>
+        <SectionContent variant='vertical'>
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
                 {Array(3).fill(true).map((data: any, id: number) => (
                     <Card key={id} className="w-full relative overflow-hidden flex flex-col justify-center gap-1 p-4">
@@ -67,41 +67,44 @@ export default function Insight({ wallets }: Props) {
             </SectionHeader>
             <SectionContent variant='vertical'>
                 <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 w-full">
-                    <Card className="w-full relative overflow-hidden flex flex-col justify-center gap-1 p-4">
+                    <Card >
                         <CardHeader>
                             <CardTitle>Smart Money</CardTitle>
                         </CardHeader>
                         <CardContent>
                             {
                                 wallets.map((wallet: IWallet, i) => (
-                                    <div key={wallet.walletAddress} className='flex items-start justify-between mb-4'>
-                                        <Avatar >
-                                            <AvatarFallback>{i + 1}</AvatarFallback>
-                                        </Avatar>
-                                        <div className='flex flex-col items-start justify-center gap-2'>
-                                            <Copy className='text-muted-foreground'
-                                                href={`/wallet/${wallet.walletAddress}`}
-                                                text={minifyContract(wallet.walletAddress)}
-                                                value={wallet.walletAddress} />
-                                            <span className={clsx('text-base-content whitespace-nowrap', wallet.netProfit > 0 ? "text-green-300" : "text-red-300")}>
-                                                {separate3digits(wallet.netProfit.toFixed(2))}
-                                            </span>
+                                    <>
+                                        <div key={wallet.walletAddress} className='flex items-start justify-between hover:bg-muted/50 hover:cursor-pointer py-3'>
+                                            <Avatar >
+                                                <AvatarFallback>{i + 1}</AvatarFallback>
+                                            </Avatar>
+                                            <div className='flex flex-col items-start justify-center gap-2'>
+                                                <Copy className='text-muted-foreground'
+                                                    href={`/wallet/${wallet.walletAddress}`}
+                                                    text={minifyContract(wallet.walletAddress)}
+                                                    value={wallet.walletAddress} />
+                                                <span className={clsx('text-base-content whitespace-nowrap leading-none', wallet.netProfit > 0 ? "text-green-300" : "text-red-300")}>
+                                                    {separate3digits(wallet.netProfit.toFixed(2))}
+                                                </span>
+                                            </div>
+                                            <span>{Math.ceil(wallet.winRate / 10)}/10</span>
                                         </div>
-                                        <span>{Math.ceil(wallet.winRate / 10)}/10</span>
-                                    </div>)
-                                )
+                                        {i != 2 && < Separator />}
+                                    </>
+                                ))
                             }
                         </CardContent>
                     </Card>
-                    <Card className="w-full relative overflow-hidden flex flex-col justify-center gap-1 p-4">
+                    <Card >
                         <CardHeader>
                             <CardTitle>Ai Trend Detector</CardTitle>
                         </CardHeader>
-                        <CardContent>
+                        <CardContent >
                             <span>
                                 {ai!.trend}
                             </span>
-                            <div className='mt-3 text-muted-foreground'>
+                            <div className='mt-5 text-muted-foreground'>
                                 {ai!.categoryTrend}
                             </div>
                         </CardContent>
