240979728ec8a3b9c742c71b9d9e3425adef8d4d
ahmad baghereslami <ahmad.b1995@gmail.com>
2024-06-22 19:51:07 +0330

    token page --> token/scoring nw design

--- components/features/token/Token-scoring.tsx
+++ components/features/token/Token-scoring.tsx
@@ -1,4 +1,5 @@
 import DoughnutChart from '@/components/ui/Doughnut'
+import { Progress } from '@/components/ui/progress'
 import { IToken } from '@/types/token.type'
 import React from 'react'
 
@@ -19,7 +20,17 @@ export default function TokenScoring({ token }: Props) {
         value={token?.ScoreData?.score1 + token?.ScoreData?.score2 + token?.ScoreData?.score3 + token?.ScoreData?.score4 + '/' + 1600}
       />
     } */}
-            <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1'>
+            {
+                token.ScoreData?.totalScore != undefined && (
+                    <div className='flex items-center justify-center'>
+                        <div className='w-full flex flex-col items-center justify-center gap-2'>
+                            <span>Security Score : {token.ScoreData.totalScore + '/' + 1600}</span>
+                            <Progress value={60} className='w-full max-w-[500px] h-3' />
+                        </div>
+                    </div>
+                )
+            }
+            <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1 mt-10'>
                 {token?.ScoreData?.score1 !== undefined && (
                     <DoughnutChart
                         // title={'Technical' + ' : ' + token?.ScoreData?.score1}
