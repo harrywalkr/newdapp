77eea22bdcd3dee38a85cc9e1b30e4cab2b9649f
ahmad baghereslami <ahmad.b1995@gmail.com>
2024-06-22 18:44:24 +0330

    token page --> token/markets tab design

--- components/features/token/Token-markets.tsx
+++ components/features/token/Token-markets.tsx
@@ -6,6 +6,7 @@ import { IToken } from '@/types/token.type'
 import { Progress } from '@/components/ui/progress'
 import TokenSecurityBox from './TokenSecurity-old-shit/TokenSecurityBox'
 import TokenSecurityCallActivity from './TokenSecurity-old-shit/TokenSecurityCallActivity'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
 interface Props {
     token: IToken,
@@ -15,49 +16,18 @@ interface Props {
 export default function TokenMarkets({ token, tokenAddress }: Props) {
     return (
         <div className='flex flex-col items-start justify-center gap-10'>
-            <div className='flex items-center justify-between w-full'>
-                <Renounce
-                    active={token.FunctionCallsData?.renounceOwnership?.status === "renounced"}
-                    text={token.FunctionCallsData?.renounceOwnership?.Date !== undefined
-                        ? `Contract was Renounced at ${token.FunctionCallsData?.renounceOwnership?.Date}`
-                        : `Contract is not Renounced`}
-                    status={token?.ScoreData?.status || ''}
-                />
-                {
-                    token.ScoreData?.totalScore != undefined && (
-                        <div className='w-full max-w-[500px] flex flex-col items-center justify-center gap-2'>
-                            <span>Security Score : {token.ScoreData.totalScore + '/' + 1600}</span>
-                            <Progress value={60} className='w-full max-w-[500px] h-3' />
-                        </div>
-                    )
-                }
-            </div>
-            {
-                token.FunctionCallsData?.malFunc?.count != undefined &&
-                token.FunctionCallsData?.malFunc?.uniqueNames != undefined &&
-                token.FunctionCallsData?.malFunc?.similarNames != undefined &&
-                <div className='flex items-center justify-between w-full gap-5 '>
-                    <h3 className='text-red-400 text-xl whitespace-nowrap'>
-                        {token.FunctionCallsData.malFunc.count} Malicious Functions
-                    </h3>
-                    <ul className='text-red-400 w-full'>
-                        {[
-                            ...token.FunctionCallsData.malFunc.uniqueNames,
-                            ...token.FunctionCallsData.malFunc.similarNames,
-                        ]?.map((un: string, idx: number) => (
-                            <li key={idx} className='inline-block m-2 px-4 py-1 rounded-sm border border-red-400'>
-                                {un}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            }
-
-            <TokenSecurityBox tokenAddress={tokenAddress} />
-            <TokenSecurityCallActivity tokenAddress={tokenAddress} />
-
-            {/* <CentralizedExchange token={token} />
-            <DecentralizedExchange token={token} /> */}
+            <Tabs defaultValue="account" className="w-full">
+                <TabsList>
+                    <TabsTrigger value="account">DEX</TabsTrigger>
+                    <TabsTrigger value="password">CEX</TabsTrigger>
+                </TabsList>
+                <TabsContent value="account">
+                    <DecentralizedExchange token={token} />
+                </TabsContent>
+                <TabsContent value="password">
+                    <CentralizedExchange token={token} />
+                </TabsContent>
+            </Tabs>
         </div>
 
     )
--- components/features/token/TokenSecurity-old-shit/TokenSecurity.tsx
+++ components/features/token/TokenSecurity-old-shit/TokenSecurity.tsx
@@ -2,6 +2,8 @@ import TokenSecurityCallActivity from "./TokenSecurityCallActivity";
 import TokenSecurityBox from "./TokenSecurityBox";
 import TokenSecurityMeasureLine from "./TokenSecurityMeasureLine";
 import { IToken } from "@/types/token.type";
+import { Progress } from "@/components/ui/progress";
+import Renounce from "../Renounce";
 
 
 interface Props {
@@ -11,10 +13,48 @@ interface Props {
 
 export default function TokenSecurityOldShit({ token, tokenAddress }: Props) {
   return (
-    <div className="flex flex-col mt-5">
-      <TokenSecurityMeasureLine token={token} tokenAddress={tokenAddress} />
+    <div className='flex flex-col items-start justify-center gap-10'>
+      <div className='flex items-center justify-between w-full'>
+        <Renounce
+          active={token.FunctionCallsData?.renounceOwnership?.status === "renounced"}
+          text={token.FunctionCallsData?.renounceOwnership?.Date !== undefined
+            ? `Contract was Renounced at ${token.FunctionCallsData?.renounceOwnership?.Date}`
+            : `Contract is not Renounced`}
+          status={token?.ScoreData?.status || ''}
+        />
+        {
+          token.ScoreData?.totalScore != undefined && (
+            <div className='w-full max-w-[500px] flex flex-col items-center justify-center gap-2'>
+              <span>Security Score : {token.ScoreData.totalScore + '/' + 1600}</span>
+              <Progress value={60} className='w-full max-w-[500px] h-3' />
+            </div>
+          )
+        }
+      </div>
+      {
+        token.FunctionCallsData?.malFunc?.count != undefined &&
+        token.FunctionCallsData?.malFunc?.uniqueNames != undefined &&
+        token.FunctionCallsData?.malFunc?.similarNames != undefined &&
+        <div className='flex items-center justify-between w-full gap-5 '>
+          <h3 className='text-red-400 text-xl whitespace-nowrap'>
+            {token.FunctionCallsData.malFunc.count} Malicious Functions
+          </h3>
+          <ul className='text-red-400 w-full'>
+            {[
+              ...token.FunctionCallsData.malFunc.uniqueNames,
+              ...token.FunctionCallsData.malFunc.similarNames,
+            ]?.map((un: string, idx: number) => (
+              <li key={idx} className='inline-block m-2 px-4 py-1 rounded-sm border border-red-400'>
+                {un}
+              </li>
+            ))}
+          </ul>
+        </div>
+      }
+
       <TokenSecurityBox tokenAddress={tokenAddress} />
       <TokenSecurityCallActivity tokenAddress={tokenAddress} />
+
     </div>
   );
 }
