872a6a31c4a6887a765e1ce9dfa30e22187aa177
ahmad baghereslami <ahmad.b1995@gmail.com>
2024-07-07 22:42:11 +0330

    minor

--- providers/index.tsx
+++ providers/index.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+'use client'
+import React, { ErrorInfo } from "react";
 import { ThemeProvider } from "./theme.provider";
 import { WagmiiProvider } from "./wagmi.provider";
 import { GlobalContextProvider } from "./context.provider";
 import QueryProvider from "./query.provider";
 import { Toaster } from "@/components/ui/toast/toaster";
+import { ErrorBoundary } from "react-error-boundary";
+import { postError } from "@/services/http/error.http";
 
-//FIXME: implement chain provider. Maybe using thirdWeb (npm)
 export default function Providers({ children }: { children: React.ReactNode }) {
+
+  const logError = (error: Error, info: ErrorInfo) => {
+    postError({ data: error })
+  };
+
   return (
     <ThemeProvider
       attribute="class"
@@ -18,11 +25,23 @@ export default function Providers({ children }: { children: React.ReactNode }) {
         {/* FIXME: remove react context provider */}
         <GlobalContextProvider>
           <QueryProvider>
-            {children}
+            <ErrorBoundary FallbackComponent={Fallback} onError={logError}>
+              {children}
+            </ErrorBoundary>
           </QueryProvider>
         </GlobalContextProvider>
         <Toaster />
       </WagmiiProvider>
     </ThemeProvider>
+  );
+}
+
+
+function Fallback({ error, resetErrorBoundary }: any) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre style={{ color: "red" }}>{error.message}</pre>
+    </div>
   );
 }
